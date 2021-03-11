e60e6ba6a1b649e8a3ada6874d9829e8d6e08c47
Irina Salovskaya <salovskaya@yandex.ru>
2021-03-11 11:44:53 +0300

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 100;
+        int bonus = 20;
+
+        int amount = cost/bonus;
+        System.out.println(amount);
+    }
+}
+
